c0929524d98c4e986b2755f61caec0eb1319c0c0
meghachaudhari906 <meghachaudhari906@gmail.com>
2025-08-29 22:52:19 +0530

    fix changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import Recipe from "./Components/Recipe-Ideas/Recipe.jsx";
-import "./components/Recipe-Ideas/Recipe.css";
+import "./Components/Recipe-Ideas/Recipe.css";
 
 
 createRoot(document.getElementById('root')).render(
